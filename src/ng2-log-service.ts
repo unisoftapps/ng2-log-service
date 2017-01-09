9f4e168deeb4d36caec9c0d7ec3669bc6ed9b79e
Brian Teryek <bteryek@sufs.org>
2017-01-09 14:32:17 -0500

    Added missing include of extension-listener

--- src/ng2-log-service.ts
+++ src/ng2-log-service.ts
@@ -5,5 +5,6 @@ export * from './log-service-provider';
 export * from './log-observer-provider';
 export * from './log-helpers';
 export * from './listeners/console-listener';
+export * from './listeners/extension-listener';
 
 export * from './log.module';
